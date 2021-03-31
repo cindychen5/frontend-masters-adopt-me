af402d01d13d1266d51656a9144b965b5d57c6a0
Cindy Chen <cchen4@cnm.edu>
2021-03-31 16:03:45 -0600

    created custom hooks for dropdown

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,20 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
+import useDropdown from "./useDropdown";
+
 
 const SearchParams = () => {
   // this is a hook; all hooks begin w/ "use"
-  const [location, setLocation] = useState('Seattle, WA');
+  const [location, setLocation] = useState("Seattle, WA");
+  const [breeds, setBreeds] = useState([]);
+  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
+
 
   return (
     <div className="search-params">
       <form>
         <label htmlFor="location">
           Location
-          <input id="location" value={location} placeholder="Location" onChange={event => setLocation(event.target.value)}/>
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={event => setLocation(event.target.value)} />
         </label>
+        <AnimalDropdown />
+        <BreedDropdown />
         <button>Submit</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
 export default SearchParams;
