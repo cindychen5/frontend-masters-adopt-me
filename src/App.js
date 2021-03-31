de6774acbcef0f7ba56888caacc3b5b26fb7d31a
Cindy Chen <cchen4@cnm.edu>
2021-03-31 14:57:28 -0600

    changed out pet names for search params

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-  return React.createElement("div", { id: "something-important" }, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Bobo",
-      animal: "Dog",
-      breed: "Pug",
-    }),
-    React.createElement(Pet, {
-      name: "Dingo",
-      animal: "Cat",
-      breed: "Black cat",
-    }),
-    React.createElement(Pet, {
-      name: "NeeNee",
-      animal: "Cat",
-      breed: "Long white hair cat",
-    }),
-  ]);
+
+  return (
+    <div>
+      <h1 id="something-important">Adopt Me!</h1>
+      <SearchParams/>
+    </div>
+  )
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App/>, document.getElementById("root"));
