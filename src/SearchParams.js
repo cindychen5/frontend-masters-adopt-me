14739b1aaafb7cb61f7a1383a558e041d1e7c02b
Cindy Chen <cchen4@cnm.edu>
2021-04-18 17:12:30 -0600

    declaring effects

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { ANIMALS } from "@frontendmasters/pet";
+import React, { useState, useEffect } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
 import useDropdown from "./useDropdown";
 
 
@@ -8,7 +8,22 @@ const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
-  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
+  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+
+  // schedules the function to run after render happens
+  // useEffect(() => {
+  //   pet.breeds("dog").then(console.log, console.error);
+  // })
+  useEffect(() => {
+    setBreeds([]);
+    setBreed("");
+
+    pet.breeds(animal).then(({ breeds }) => {
+      const breedStrings = breeds.map(({name}) => name);
+      setBreeds(breedStrings);
+    }, console.error);
+  }, [animal, setBreed, setBreeds])
+
 
 
   return (
